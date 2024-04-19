9a78c9aa394d582b4cfc2b698b05d2d7af5bac4e
jonathan <jonathan.stefan@inatel.br>
2024-04-19 20:03:46 -0300

    add chamada de funções

--- main/main.py
+++ main/main.py
@@ -3,52 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control as ctrl
 import sys
-sys.path.append('identificacao')  # Adiciona o diretório 'funcoes' ao caminho de busca
+sys.path.append('identificacao')  # Adiciona o diretório 'identificacao' ao caminho de busca
 from Smith import Smith
 from Sundaresan import Sundaresan
+import sys
+sys.path.append('funcoes')  # Adiciona o diretório 'funcoes' ao caminho de busca
+from Funcoes import Pid, CHR20, CohenCoon
 import tkinter as tk
 from tkinter import ttk
 # Carrega o dataset 
 mat=loadmat('dataset/Dataset_Grupo4.mat')
-
-# Função chamada quando o botão é pressionado
-def atualizar():
-    Kp = float(entry_kp.get())
-    Ti = float(entry_ti.get())
-    Td = float(entry_td.get())
-    
-    # Calcular a resposta ao degrau
-    calcular_resposta(Kp, Ti, Td)
-    
-# Criar a janela principal
-root = tk.Tk()
-root.title("Controle PID")
-
-# Criar os campos de entrada e etiquetas para Kp, Ti e Td
-label_kp = ttk.Label(root, text="Kp:")
-label_kp.grid(row=0, column=0, padx=5, pady=5)
-entry_kp = ttk.Entry(root)
-entry_kp.grid(row=0, column=1, padx=5, pady=5)
-entry_kp.insert(0, "1.0")  # Valor padrão
-
-label_ti = ttk.Label(root, text="Ti:")
-label_ti.grid(row=1, column=0, padx=5, pady=5)
-entry_ti = ttk.Entry(root)
-entry_ti.grid(row=1, column=1, padx=5, pady=5)
-entry_ti.insert(0, "1.0")  # Valor padrão
-
-label_td = ttk.Label(root, text="Td:")
-label_td.grid(row=2, column=0, padx=5, pady=5)
-entry_td = ttk.Entry(root)
-entry_td.grid(row=2, column=1, padx=5, pady=5)
-entry_td.insert(0, "1.0")  # Valor padrão
-
-# Botão para calcular e atualizar a resposta ao degrau
-button_atualizar = ttk.Button(root, text="Atualizar", command=atualizar)
-button_atualizar.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
-
-# Mostrar a janela
-root.mainloop()
 
 # Variáveis
 struct_degrau = mat.get('TARGET_DATA____ProjetoC213_Degrau')
@@ -91,13 +55,10 @@ else :
     tau = sundaresan[1]
     theta = sundaresan[2]
    
-
-print("valor de k:", k, tau, theta)
-
 # Criar o sistema de controle
 sys = ctrl.TransferFunction([k], [tau, 1])
 
-n_pade = 6
+n_pade = 10
 ( num_pade , den_pade ) = ctrl.pade ( theta , n_pade )
 H_pade = ctrl.TransferFunction( num_pade , den_pade )
 
@@ -105,15 +66,10 @@ H_pade = ctrl.TransferFunction( num_pade , den_pade )
 sys_atraso = ctrl.series(sys, H_pade)
 
 # Modelo CHR com 20% de sobrevalor
-
-Kp = 0.95 * tau / (k * theta)
-Ti = 1.357 * tau
-Td = 0.4730 * theta
+chr = CHR20 (theta, tau, k)
 
 # Criar a função de transferência do controlador PID
-num_pid = [Kp * Td, Kp, Kp / Ti]
-den_pid = [1, 0]
-PID = ctrl.TransferFunction(num_pid, den_pid)
+PID = Pid (chr[0], chr[1], chr[2])
 Cs = ctrl.series(PID, sys_atraso) # Série de PID e sistema com atraso
 
 # Plotar a resposta ao degrau da malha fechada
@@ -127,21 +83,16 @@ plt.legend(loc='lower right')
 plt.grid(True)
 
 # Modelo Cohen e Coon
-
-Kpc = (tau / (k * theta)) * ((16 * tau + 30) / (12 * tau))
-Tic = theta * ((32 + 6 * theta / tau) / (13 + (8 * theta / tau)))
-Tdc = 4 * theta / (11 + 2 * theta / tau)
+cohenCoon = CohenCoon(theta, tau, k)
 
 # Criar a função de transferência do controlador PID usando o metodo Cohen Coon
-num_pidc = [Kpc * Tdc, Kpc * (1 + 0.5 * theta / tau), Kpc * theta / (2 * tau)] 
-den_pidc = [1, 0]
-PIDc = ctrl.TransferFunction(num_pidc, den_pidc)
-Csc = ctrl.series(PIDc, sys_atraso) # Série de PID e sistema com atraso
+PID = Pid (cohenCoon[0], cohenCoon[1], cohenCoon[2])
+Cs = ctrl.series(PID, sys_atraso) # Série de PID e sistema com atraso
 
 # Plotar a resposta ao degrau da malha fechada
 plt.figure()
-t_cc, y_cc = ctrl.step_response(ctrl.feedback(Csc, 1))
-plot2 = plt.plot(t_cc, y_cc, label='Cohen-Coon')
+t, y = ctrl.step_response(ctrl.feedback(Cs, 1))
+plt.plot(t, y, label='Cohen-Coon')
 plt.xlabel('Tempo')
 plt.ylabel('Amplitude')
 plt.title('Resposta ao degrau da malha fechada')
@@ -149,6 +100,21 @@ plt.legend(loc='lower right')
 plt.grid(True)
 
 plt.show()
+
+# Função para calcular a resposta ao degrau
+def calcular_resposta(Kp, Ti, Td):
+    PID = Pid(Kp, Ti, Td)
+    Cs = ctrl.series(PID, sys_atraso) # Série de PID e sistema com atraso
+    plt.figure()
+    t, y = ctrl.step_response(ctrl.feedback(Cs, 1))
+    plt.plot(t, y, label = 'Ajuste fino')
+    plt.xlabel('Tempo')
+    plt.ylabel('Amplitude')
+    plt.title('Resposta ao degrau da malha fechada Kp, Ti, Td adicionados pelo usuario')
+    plt.legend(loc='lower right')
+    plt.grid(True)
+    plt.show()
+    pass
 
 # Função chamada quando o botão é pressionado
 def atualizar():
@@ -162,6 +128,21 @@ def atualizar():
 # Criar a janela principal
 root = tk.Tk()
 root.title("Controle PID")
+
+# Definir o tamanho da janela
+largura_janela = 400
+altura_janela = 300
+
+# Obter a largura e a altura da tela
+largura_tela = root.winfo_screenwidth()
+altura_tela = root.winfo_screenheight()
+
+# Calcular as coordenadas para colocar a janela no centro da tela
+x_pos = largura_tela // 2 - largura_janela // 2
+y_pos = altura_tela // 2 - altura_janela // 2
+
+# Definir a geometria da janela
+root.geometry(f'{largura_janela}x{altura_janela}+{x_pos}+{y_pos}')
 
 # Criar os campos de entrada e etiquetas para Kp, Ti e Td
 label_kp = ttk.Label(root, text="Kp:")
